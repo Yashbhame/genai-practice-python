6399ade381b2c8104f8e3d989605e6982e4a8646
Yashbhame_018 <yashbhame27@gmail.com>
2026-06-24 22:49:16 +0530

    Week 1 Day 1:Datatypes

---
+++ week01-python-basics/day1_Datatypes.py
@@ -0,0 +1,50 @@
+#1
+print("Hello Yash")
+
+#2
+num1 = 10
+print(num1)
+print(type(num1))
+
+#3
+num2 = 20.5
+print(num2)
+print(type(num2))
+
+#4
+num3 = 30 + 2j
+print(num3)
+print(type(num3))
+
+#5
+name = "Yash"
+print(name)
+print(type(name))
+
+#6
+is_student = True
+print(is_student)
+print(type(is_student))
+
+#7
+a = 2
+b = 3.5
+print(a + b)
+
+#8
+list1 = ["English", "Maths", "Science", "History", "Geography"]
+print(list1[1])
+print(list1[0:3])
+print(list1[-4])
+print(type(list1))
+
+#9
+tuple1 = ("English", "Maths", "Science", "History", "Geography")
+
+#10
+list2 = [10, 20, 30, 40, 50]
+print(list2[0]+list2[1]+list2[2]+list2[3]+list2[4])
+
+#11
+set1 = {10, 20, 30, 40, 10}
+print(set1)
