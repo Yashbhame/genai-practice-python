fb1b04bb1624dfa02fb23f280418f68c6b9d6f6d
Yashbhame_018 <yashbhame27@gmail.com>
2026-06-25 17:14:34 +0530

    Week 1 Day 1:Datatypes completed and input,output scripts added

--- week01-python-basics/day1_Datatypes.py
+++ week01-python-basics/day1_Datatypes.py
@@ -46,5 +46,40 @@ list2 = [10, 20, 30, 40, 50]
 print(list2[0]+list2[1]+list2[2]+list2[3]+list2[4])
 
 #11
-set1 = {10, 20, 30, 40, 10}
+set1 = {10, 20, 30, 40, 10}   # Duplicate values are automatically removed
 print(set1)
+
+#12
+list3 = ["English", "Maths", "Science", "History", "Geography"]
+list3[3] = "C2W"
+print(list3)
+
+#13
+dict1 = {"name": "Yash", "age": 22, "marks": 9.25}
+print(dict1["name"])
+print(dict1["age"])
+print("marks:", dict1["marks"])
+dict1["marks"] = 9.5
+print(dict1["marks"])
+print(type(dict1["marks"]))
+
+
+#14
+dict2 = {0:10, 1:20, 2:30, 3:40, 4:50}
+key = 2
+print(key in dict2)
+'''if key in dict2:
+    print("Key exists in the dictionary")
+    print("Value:", dict2[key])
+else:
+    print("Key does not exist in the dictionary")
+    '''
+
+#15
+print(dict2[2]+dict2[3])
+
+#16
+string1 = "Yash"
+string2 = 'C2W'
+string3 = """This is a multi-line string.
+It can span multiple lines."""
---
+++ week01-python-basics/day1_I_O.py
@@ -0,0 +1,42 @@
+#1
+num = int(input("Enter a number: "))
+print("You entered:", num)
+
+#2
+name = input("Enter your name: ")
+print("Hello,", name)
+
+#3
+fnum = float(input("Enter a floating-point number: "))
+print("You entered:", fnum)
+
+#4
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print("Sum:", num1 + num2)
+
+#5
+float1 = float(input("Enter first floating-point number: "))
+float2 = float(input("Enter second floating-point number: "))
+float3 = float(input("Enter third floating-point number: "))
+print("Average:", (float1 + float2 + float3) / 3)
+
+#6
+number = int(input("Enter number and know its type: "))
+print("You entered:", number)
+print("Type:", type(number))
+
+#7
+text = input("Enter a string: ")
+print("You entered:", text)
+print("Type:", type(text))
+
+#8
+float_num = float(input("Enter a floating-point number: "))
+print("You entered:", float_num)
+print("Type:",type(float_num))
+
+#9
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+print(number1,"*",number2,"=",number1*number2)
